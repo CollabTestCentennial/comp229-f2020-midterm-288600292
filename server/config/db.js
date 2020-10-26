47a6bd139ca267121d42b0686401784494570b37
skamino <kaminostephen@yahoo.ca>
2020-10-26 12:56:03 -0400

    added some admin stuff

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,6 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/books229"
+  //"URI": "mongodb://localhost/books229"
+  "URI": "mongodb+srv://skamino:RuVGzNNtzp5d0lTt@comp229-a2.pqjcv.mongodb.net/MySite?retryWrites=true&w=majority"
+
 };
