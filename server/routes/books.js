f573e5fd625904d9db9c8e3f1df8fbd6ff976edb
skamino <kaminostephen@yahoo.ca>
2020-10-26 12:28:44 -0400

    fixed the add button

--- server/routes/books.js
+++ server/routes/books.js
@@ -23,22 +23,34 @@ router.get('/', (req, res, next) => {
 
 });
 
-//  GET the Book Details page in order to add a new Book
+//  GET the Book Add page in order to add a new Book
 router.get('/add', (req, res, next) => {
-
-    /*****************
-     * ADD CODE HERE *
-     *****************/
-
+  res.render('books/add', {title: 'Add Book', books: ''})
 });
 
-// POST process the Book Details page and create a new Book - CREATE
+// POST calls the function that adds the entry to mongo
 router.post('/add', (req, res, next) => {
-
-    /*****************
-     * ADD CODE HERE *
-     *****************/
-
+  //the main thing here is the reflection of the model
+  let newBook = book({
+    "Title": req.body.Title,
+    "Description": req.body.Description,
+    "Price": req.body.Price,
+    "Author": req.body.Author,
+    "Genre": req.body.Genre
+  })
+  //this is the adding of "newBook" varaible
+  book.create(newBook, (err, Book) =>{
+    if(err)
+    {
+        console.log(err);
+        res.end(err);
+    }
+    else
+    {
+        // ensures that the new data is rendered
+        res.redirect('/books');
+    }
+});
 });
 
 // GET the Book Details page in order to edit an existing Book
